c37f4f29bddf17e473acb70aa4cf52b6bc94a094
Leoschmittk <Leoeyeschmittk@gmail.com>
2025-10-09 20:42:50 -0300

    feat: first commit hello world

---
+++ src/main/java/com/controle_estoque_back/controle_estoque_back/ControleEstoqueBackApplication.java
@@ -0,0 +1,14 @@
+package com.controle_estoque_back.controle_estoque_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstoqueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstoqueBackApplication.class, args);
+		System.out.println("Hello, world\r");
+	}
+
+}
---
+++ src/test/java/com/controle_estoque_back/controle_estoque_back/ControleEstoqueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle_estoque_back.controle_estoque_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
