7cd4d765c373c1be595f6b8dbc605d5ac8db808e
leo150406 <leonardo.sousavargas150406@gmail.com>
2025-11-17 21:40:53 -0300

    feat: created category class

---
+++ src/main/java/com/controle_estoque_back/entity/Category.java
@@ -0,0 +1,10 @@
+package com.controle_estoque_back.entity;
+
+public class Category {
+    private Long id;
+    private String name;
+    private String size;      
+    private String packaging; 
+
+    
+}
