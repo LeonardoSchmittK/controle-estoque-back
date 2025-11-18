e980e9e0f44a93acb99106198d4372e0f13e4a79
leo150406 <leonardo.sousavargas150406@gmail.com>
2025-11-17 21:51:59 -0300

    feat: creating getters and setters

--- src/main/java/com/controle_estoque_back/entity/Category.java
+++ src/main/java/com/controle_estoque_back/entity/Category.java
@@ -6,5 +6,46 @@ public class Category {
     private String size;      
     private String packaging; 
 
-    
+    public Category() {
+    }
+
+    public Category(Long id, String name, String size, String packaging) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.packaging = packaging;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {   // <— ✔ THIS FIXES THE ERROR
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getPackaging() {
+        return packaging;
+    }
+
+    public void setPackaging(String packaging) {
+        this.packaging = packaging;
+    }
+
 }
