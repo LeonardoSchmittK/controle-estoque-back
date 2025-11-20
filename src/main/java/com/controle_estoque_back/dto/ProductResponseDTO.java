0e4d9f1509ba3e1227b754301926ce28c2020d30
davidcrech <davidcrech220@gmail.com>
2025-11-20 02:13:24 -0300

    feat: add req/res Product DTOs

--- src/main/java/com/controle_estoque_back/dto/ProductResponseDTO.java
+++ src/main/java/com/controle_estoque_back/dto/ProductResponseDTO.java
@@ -1,4 +1,76 @@
 package com.controle_estoque_back.dto;
 
 public class ProductResponseDTO {
+    private Long id;
+    private String name;
+    private Double unitPrice;
+    private String unit;
+    private Integer quantityInStock;
+    private Integer minQuantity;
+    private Integer maxQuantity;
+    private Long categoryId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Integer getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(Integer minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    public Integer getMaxQuantity() {
+        return maxQuantity;
+    }
+
+    public void setMaxQuantity(Integer maxQuantity) {
+        this.maxQuantity = maxQuantity;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 }
