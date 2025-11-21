8a486d4e194f5d06a9b26efd675fbb597af69067
GabrielMartinsMeira <martinsmeiragabriel@gmail.com>
2025-11-21 20:05:27 -0300

    feat: Creating CategoryRepository and fixing Category entity

--- src/main/java/com/controle_estoque_back/entity/Category.java
+++ src/main/java/com/controle_estoque_back/entity/Category.java
@@ -1,6 +1,14 @@
 package com.controle_estoque_back.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private Long id;
     private String name;
     private String size;      
