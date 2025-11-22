e0f8fd0fd72c3da968c2876cf73b139743eafb0b
davidcrech <davidcrech220@gmail.com>
2025-11-22 01:33:07 -0300

    feat: add Movement entity/type

---
+++ src/main/java/com/controle_estoque_back/entity/Movement.java
@@ -0,0 +1,62 @@
+package com.controle_estoque_back.entity;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+
+@Entity
+public class Movement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Integer quantityMoved;
+
+    @Enumerated(EnumType.STRING)
+    private MovementType movementType;
+
+    private Instant movementDate = Instant.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantityMoved() {
+        return quantityMoved;
+    }
+
+    public void setQuantityMoved(Integer quantityMoved) {
+        this.quantityMoved = quantityMoved;
+    }
+
+    public MovementType getMovementType() {
+        return movementType;
+    }
+
+    public void setMovementType(MovementType movementType) {
+        this.movementType = movementType;
+    }
+
+    public Instant getMovementDate() {
+        return movementDate;
+    }
+
+    public void setMovementDate(Instant movementDate) {
+        this.movementDate = movementDate;
+    }
+}
+
