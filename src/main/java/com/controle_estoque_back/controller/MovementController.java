9d1728111c053fb240811fad098c242025576c25
davidcrech <davidcrech220@gmail.com>
2025-11-22 02:27:11 -0300

    feat: add MovementController

---
+++ src/main/java/com/controle_estoque_back/controller/MovementController.java
@@ -0,0 +1,30 @@
+package com.controle_estoque_back.controller;
+
+import com.controle_estoque_back.dto.MovementRequestDTO;
+import com.controle_estoque_back.dto.MovementResponseDTO;
+import com.controle_estoque_back.service.MovementService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movements")
+public class MovementController {
+
+    private final MovementService service;
+
+    public MovementController(MovementService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovementResponseDTO>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<MovementResponseDTO> create(@RequestBody MovementRequestDTO dto) {
+        return ResponseEntity.ok(service.save(dto));
+    }
+}
