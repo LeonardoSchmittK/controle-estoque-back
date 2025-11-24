846905bf047ddd45c0443d00f4c5aa755ed5b93a
GabrielMartinsMeira <martinsmeiragabriel@gmail.com>
2025-11-23 21:28:58 -0300

    chore:Add the bean validation to RequestDTO files

--- src/main/java/com/controle_estoque_back/dto/CategoryRequestDTO.java
+++ src/main/java/com/controle_estoque_back/dto/CategoryRequestDTO.java
@@ -1,10 +1,22 @@
 package com.controle_estoque_back.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class CategoryRequestDTO {
 
+    @NotNull(message = "O ID não pode ser nulo")
     private Long id;
+
+    @NotBlank(message = "O nome da categoria é obrigatório")
+    @Size(min = 2, max = 50, message = "O nome deve ter entre 2 e 50 caracteres")
     private String name;
+
+    @NotBlank(message = "O tamanho deve ser informado")
     private String size;
+
+    @NotBlank(message = "A embalagem deve ser informada")
     private String packaging;
 
     public Long getId() {
--- src/main/java/com/controle_estoque_back/dto/MovementRequestDTO.java
+++ src/main/java/com/controle_estoque_back/dto/MovementRequestDTO.java
@@ -1,10 +1,18 @@
 package com.controle_estoque_back.dto;
 
 import com.controle_estoque_back.entity.MovementType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record MovementRequestDTO(
+        @NotNull(message = "O ID do produto é obrigatório")
         Long productId,
+
+        @NotNull(message = "A quantidade movimentada é obrigatória")
+        @Positive(message = "A quantidade deve ser maior que zero")
         Integer quantityMoved,
+
+        @NotNull(message = "O tipo de movimentação (ENTRY ou EXIT) é obrigatório")
         MovementType movementType
 ) {}
 
